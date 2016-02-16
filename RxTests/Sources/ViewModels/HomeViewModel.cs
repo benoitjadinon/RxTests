6be0c7463a1aac2ab6fb6c112d8c7f490b03f651
Benoit Jadinon <j@dinonbeno.it>
2016-02-16 15:57:09 +0100

    showing 10 for a second before timer + fixed dispose on manual cancel

--- RxTests/Sources/ViewModels/HomeViewModel.cs
+++ RxTests/Sources/ViewModels/HomeViewModel.cs
@@ -20,25 +20,37 @@ namespace RxTests
 				.Open ();
 
 			var onOKPressed = Observable.FromEventPattern<EventHandler, EventArgs> (h => alert.OnOK += h, h => alert.OnOK -= h);
-			onOKPressed.Subscribe (_ => Go ());
-
 			var onCancelPressed = Observable.FromEventPattern<EventHandler, EventArgs> (h => alert.OnCancel += h, h => alert.OnCancel -= h);
-			onCancelPressed.Subscribe (_ => Cancel ());
 
-			Observable
-				.Interval (TimeSpan.FromMilliseconds (TimerIntervalMillisec))
-				.Take (TimerStartTime + 1) // +1 to show 0 for a second
-				.Select(p => TimerStartTime-p) // inverse countdown
+			var loop = Observable.Return (TimerStartTime) //to show the first '10' while waiting for 1st loop
+				.Select (p => Convert.ToInt64(p))
+				.Merge (
+					Observable.Interval (TimeSpan.FromMilliseconds (TimerIntervalMillisec))
+					.Take (TimerStartTime + 1)           // +1 to show '0' for a second
+					.Select(p => TimerStartTime - p - 1) // inverse countdown
+				)
 				.TakeUntil (onOKPressed)
 				.TakeUntil (onCancelPressed)
 				.ObserveOn (SynchronizationContext.Current)
 				.Subscribe (
-					onNext: seconds => alert.SetTimeRemaining (millisec: seconds * TimerIntervalMillisec),
+					onNext: seconds => {
+						if (seconds >= 0) 
+							alert.SetTimeRemaining (millisec: seconds * TimerIntervalMillisec);
+					},
 					onCompleted: () => {
 						alert.Close ();
 						Go ();
 					}
 				);
+			
+			onOKPressed.Subscribe (_ => {
+				loop.Dispose();
+				Go ();
+			});
+			onCancelPressed.Subscribe (_ => {
+				loop.Dispose();
+				Cancel ();
+			});
 		}
 
 		void Cancel ()
